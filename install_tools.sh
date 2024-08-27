dd7f3fa2d580631a8cf66cbbe7471b570262780f
Olaleye <90139191+Junnygram@users.noreply.github.com>
2024-08-27 12:31:14 +0100

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -9,17 +9,22 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
-# Install eksctl
-echo "Installing eksctl..."
-curl -s "https://api.github.com/repos/weaveworks/eksctl/releases/latest" | \
-grep "browser_download_url.*linux_amd64.tar.gz" | \
-cut -d : -f 2,3 | \
-tr -d \" | \
-wget -qi -
-tar -xzf eksctl_*.tar.gz -C /tmp && rm eksctl_*.tar.gz
-sudo mv /tmp/eksctl /usr/local/bin
-eksctl version
 
+
+
+
+# Install eksctl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo mv /tmp/eksctl /usr/local/bin
 # Install AWS CLI v2
 echo "Installing AWS CLI v2..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
