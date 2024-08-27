c09cd4031ea934ca7f632874ac9338cc57147d08
Olaleye <90139191+Junnygram@users.noreply.github.com>
2024-08-27 13:15:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,6 +25,9 @@ curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_ch
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 
 sudo mv /tmp/eksctl /usr/local/bin
+
+
+
 # Install AWS CLI v2
 echo "Installing AWS CLI v2..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
